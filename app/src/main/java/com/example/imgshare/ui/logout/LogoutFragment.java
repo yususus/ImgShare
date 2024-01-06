85c252edf3354aaa779c0c3641600f20c21e52b8
Yusuf Aydın <yusayd3825@gmail.com>
2024-01-06 22:07:32 +0300

    created about and logout view

--- app/src/main/java/com/example/imgshare/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/imgshare/ui/logout/LogoutFragment.java
@@ -1,66 +1,48 @@
 package com.example.imgshare.ui.logout;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.imgshare.Login;
+import com.example.imgshare.MainActivity;
+import com.example.imgshare.OpenView;
 import com.example.imgshare.R;
+import com.example.imgshare.databinding.FragmentAboutBinding;
+import com.example.imgshare.databinding.FragmentLogoutBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LogoutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LogoutFragment extends Fragment {
+    private FragmentLogoutBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LogoutFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogoutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LogoutFragment newInstance(String param1, String param2) {
-        LogoutFragment fragment = new LogoutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logout, container, false);
+        binding = FragmentLogoutBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Button btnlogout = binding.logout;
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OpenView.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        return root;
+    }
+    @Override
+    public  void onDestroyView(){
+        super.onDestroyView();
+        binding = null;
     }
 }
